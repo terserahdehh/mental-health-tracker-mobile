aef00cd76191d3163532809b0f215caf3aad168d
terserahdehh <serafina.nala@gmail.com>
2024-11-13 12:46:23 +0700

    tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -11,57 +12,55 @@ class LeftDrawer extends StatelessWidget {
       child: ListView(
         children: [
           DrawerHeader(
-                decoration: BoxDecoration(
-      color: Theme.of(context).colorScheme.primary,
-    ),
-    child: const Column(
-      children: [
-        Text(
-          'Mental Health Tracker',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            child: const Column(
+              children: [
+                Text(
+                  'Mental Health Tracker',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  "Track your mental health every day here!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(padding: EdgeInsets.all(8)),
-        Text(
-          textAlign: TextAlign.center,
-          "Track your mental health every day here!",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.normal,
-            fontSize: 15.0,
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home Page'),
+            // Redirection part to MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            },
           ),
-        ),
-      ],
-    ),
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('Mood List'),
+            onTap: () {
+                // Route to the mood page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                );
+            },
           ),
-              ListTile(
-    leading: const Icon(Icons.home_outlined),
-    title: const Text('Home Page'),
-    // Redirection part to MyHomePage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MyHomePage(),
-          ));
-    },
-  ),
-  ListTile(
-    leading: const Icon(Icons.mood),
-    title: const Text('Add Mood'),
-    // Redirection part to MoodEntryFormPage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MoodEntryFormPage(),
-          ));
-    },
-  ),
         ],
       ),
     );
--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -1,5 +1,9 @@
-import 'package:mental_health_tracker/screens/moodentry_form.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:mental_health_tracker/screens/login.dart';
 
 class ItemHomepage {
   final String name;
@@ -9,35 +13,74 @@ class ItemHomepage {
 }
 
 class ItemCard extends StatelessWidget {
+  // Display the card with an icon and name.
+
   final ItemHomepage item;
 
   const ItemCard(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Material(
+      // Specify the background color of the application theme.
       color: Theme.of(context).colorScheme.secondary,
+      // Round the card border.
       borderRadius: BorderRadius.circular(12),
+
       child: InkWell(
-        onTap: () {
+        // Touch-responsive area
+        onTap: () async {
+          // Show SnackBar when clicked
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("You have pressed the ${item.name} button!")),
-            );
+            ..showSnackBar(SnackBar(
+                content: Text("You pressed the ${item.name} button!")));
+
+          // Navigate to the appropriate route (depending on the button type)
           if (item.name == "Add Mood") {
-            Navigator.pushReplacement(
+            Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => MoodEntryFormPage(),
-              ),
+                  builder: (context) => const MoodEntryFormPage()),
             );
+          } else if (item.name == "View Mood") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+            );
+          } 
+          else if (item.name == "Logout") {
+            final response = await request.logout(
+                "http://localhost:8000/auth/logout/");
+            String message = response["message"];
+            if (context.mounted) {
+                if (response['status']) {
+                    String uname = response["username"];
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text("$message Goodbye, $uname."),
+                    ));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                    );
+                } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(message),
+                        ),
+                    );
+                }
+            }
           }
         },
+
+        // Container to store the Icon and Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
+              // Place the Icon and Text in the center of the card.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
